2679d717eee5034efdff5e6c9eb4f69805515706
Philip Johnson <johnson@hawaii.edu>
2017-01-13 08:08:25 -1000

    Finished profile page mockup

--- app/imports/startup/client/router.js
+++ app/imports/startup/client/router.js
@@ -1,5 +1,7 @@
 import { FlowRouter } from 'meteor/kadira:flow-router';
 import { BlazeLayout } from 'meteor/kadira:blaze-layout';
+import { $ } from 'meteor/jquery';
+
 
 /*                        LANDING ROUTE                       */
 
@@ -11,11 +13,44 @@ FlowRouter.route('/', {
   },
 });
 
+/*                        USER ROUTE s                      */
+
+
+function addBodyClass() {
+  $('body').addClass('user-layout-body');
+}
+
+function removeBodyClass() {
+  $('body').removeClass('user-layout-body');
+}
+
+const userRoutes = FlowRouter.group({
+  prefix: '/user',
+  name: 'userRoutes',
+  triggersEnter: [addBodyClass],
+  triggersExit: [removeBodyClass],
+});
+
 export const profilePageRouteName = 'Profile_Page';
-FlowRouter.route('/profile', {
+userRoutes.route('/profile', {
   name: profilePageRouteName,
   action() {
     BlazeLayout.render('User_Layout', { main: profilePageRouteName });
   },
 });
+
+/*                        MISC ROUTES                       */
+FlowRouter.notFound = {
+  action() {
+    BlazeLayout.render('Page_Not_Found');
+  },
+};
+
+// export const profilePageRouteName = 'Profile_Page';
+// FlowRouter.route('/profile', {
+//   name: profilePageRouteName,
+//   action() {
+//     BlazeLayout.render('User_Layout', { main: profilePageRouteName });
+//   },
+// });
 
--- app/imports/ui/layouts/user/index.js
+++ app/imports/ui/layouts/user/index.js
@@ -1,4 +1,5 @@
-import './footer.html';
-import './header.html';
-import './header.js';
+import './user-footer.html';
+import './user-header.html';
+import './user-header.js';
+import './user-layout.css';
 import './user-layout.html';
