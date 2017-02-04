e863020e1ab9a62fcdd5ca1336397d20b8684110
Philip Johnson <johnson@hawaii.edu>
2017-02-03 15:58:07 -1000

    Fixture for database set up.

--- app/imports/startup/server/initialize-database.js
+++ app/imports/startup/server/initialize-database.js
@@ -1,15 +1,42 @@
 import { Meteor } from 'meteor/meteor';
 import { Profiles } from '/imports/api/profile/ProfileCollection';
 import { Interests } from '/imports/api/interest/InterestCollection';
+import { _ } from 'meteor/underscore';
 
 /* global Assets */
 
+/**
+ * Returns the definition array associated with collectionName in the restoreJSON structure.
+ * @param restoreJSON The restore file contents.
+ * @param collection The collection of interest.
+ */
+function getDefinitions(restoreJSON, collection) {
+  return _.find(restoreJSON.collections, obj => obj.name === collection).contents;
+}
+
+/**
+ * Given a collection and the restoreJSON structure, looks up the definitions and invokes define() on them.
+ * @param collection The collection to be restored.
+ * @param restoreJSON The structure containing all of the definitions.
+ */
+function restoreCollection(collection, restoreJSON) {
+  const definitions = getDefinitions(restoreJSON, collection._collectionName);
+  console.log(`Defining ${definitions.length} ${collection._collectionName} documents.`);
+  _.each(definitions, definition => collection.define(definition));
+}
+
 Meteor.startup(() => {
   /** Only initialize database if it's empty. */
-  const totalDocuments = Profiles.count() + Interests.count();
+  const collectionList = [Interests, Profiles];
+  const totalDocuments = _.reduce(collectionList, function reducer(memo, collection) {
+    return memo + collection.count();
+  }, 0);
   if (totalDocuments === 0) {
     const fileName = Meteor.settings.public.initialDatabaseFileName;
     console.log(`Restoring database from file ${fileName}.`);
     const restoreJSON = JSON.parse(Assets.getText(fileName));
+    _.each(collectionList, collection => {
+      restoreCollection(collection, restoreJSON);
+    });
   }
 });
