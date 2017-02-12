b8c9b4940a56b8e07fceafc7f0bb7f1e692bc213
Philip Johnson <johnson@hawaii.edu>
2017-02-12 09:01:26 -1000

    checkpoint. Hooking up filter page.

--- app/imports/ui/pages/filter/filter-page.js
+++ app/imports/ui/pages/filter/filter-page.js
@@ -1,22 +1,27 @@
 import { Template } from 'meteor/templating';
+import { ReactiveDict } from 'meteor/reactive-dict';
+import { _ } from 'meteor/underscore';
+import { Profiles } from '/imports/api/profile/ProfileCollection';
+import { Interests } from '/imports/api/interest/InterestCollection';
+
+/* eslint-disable no-param-reassign */
+
+const profileIDs = 'profileIDs';
+
+Template.Filter_Page.onCreated(function onCreated() {
+  this.subscribe(Interests.getPublicationName());
+  this.subscribe(Profiles.getPublicationName());
+  this.messageFlags = new ReactiveDict();
+  this.messageFlags.set(profileIDs, undefined);
+});
 
 Template.Filter_Page.helpers({
   profiles() {
-    return [
-      {
-        name: 'Lee Altenberg',
-        picture: 'altenberg.jpg',
-        title: 'Professor, ICS',
-        description: 'Evolutionary Computation',
-        interests: ['Algorithms'],
-      },
-      {
-        name: 'Kyungim Baek',
-        picture: 'baek.jpg',
-        title: 'Professor, ICS',
-        description: 'Computer Vision',
-        interests: ['Algorithms'],
-      },
-    ];
+    // Initialize the profiles to display to all of the existing profiles.
+    if (!Template.instance().messageFlags.get(profileIDs)) {
+      Template.instance().messageFlags.set(profileIDs, _.map(Profiles.findAll(), profile => profile._id));
+    }
+
+    return _.map(Template.instance().messageFlags.get(profileIDs), profileID => Profiles.findDoc(profileID));
   },
 });
