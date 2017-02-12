4bc10c38f06e73bee2ab92a77107a3d6355abe77
Philip Johnson <johnson@hawaii.edu>
2017-02-12 12:05:34 -1000

    finished filtering

--- app/imports/ui/pages/filter/filter-page.js
+++ app/imports/ui/pages/filter/filter-page.js
@@ -24,15 +24,17 @@ Template.Filter_Page.helpers({
     // Find all profiles with the currently selected interests.
     const allProfiles = Profiles.findAll();
     const selectedInterests = Template.instance().messageFlags.get(selectedInterestsKey);
-    return _.filter(allProfiles, profile => _.intersection(profile.interests, selectedInterests));
+    return _.filter(allProfiles, profile => _.intersection(profile.interests, selectedInterests).length > 0);
   },
 
   interests() {
     return _.map(Interests.findAll(),
-            function makeInterestObject(interest) {
-              return { label: interest.name,
-                selected: _.contains(Template.instance().messageFlags.get(selectedInterestsKey), interest.name) };
-            });
+        function makeInterestObject(interest) {
+          return {
+            label: interest.name,
+            selected: _.contains(Template.instance().messageFlags.get(selectedInterestsKey), interest.name),
+          };
+        });
   },
 });
 
