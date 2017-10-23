95781934a65f1da05bf49e1627ea0e351d0b9034
Philip Johnson <johnson@hawaii.edu>
2017-10-23 13:29:50 -1000

    Update testing.

--- app/imports/api/profile/ProfileCollection.test.js
+++ app/imports/api/profile/ProfileCollection.test.js
@@ -61,14 +61,14 @@ if (Meteor.isServer) {
 
     it('#define (illegal interest)', function test() {
       const illegalInterests = ['foo'];
-      const defineObject2 = { firstName, lastName, username, bio, illegalInterests, picture, title,
+      const defineObject2 = { firstName, lastName, username, bio, interests: illegalInterests, picture, title,
         github, facebook, instagram };
       expect(function foo() { Profiles.define(defineObject2); }).to.throw(Error);
     });
 
     it('#define (duplicate interests)', function test() {
       const duplicateInterests = [interestName, interestName];
-      const defineObject3 = { firstName, lastName, username, bio, duplicateInterests, picture, title,
+      const defineObject3 = { firstName, lastName, username, bio, interests: duplicateInterests, picture, title,
         github, facebook, instagram };
       expect(function foo() { Profiles.define(defineObject3); }).to.throw(Error);
     });
