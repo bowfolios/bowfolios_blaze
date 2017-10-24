bd1d74840afdc32c0860b099a7a8b7ae29d19ee6
Philip Johnson <johnson@hawaii.edu>
2017-10-24 11:42:24 -1000

    checkpoint.
    I’m stopping work on this because it’s not simple to directly use the
    semantic-ui.css and .js files.

--- app/client/main.js
+++ app/client/main.js
@@ -11,7 +11,8 @@ import '/imports/ui/pages/directory';
 import '/imports/ui/pages/filter';
 import '/imports/ui/pages/landing';
 import '/imports/ui/pages/user';
-import '/imports/ui/stylesheets/style.css';
 import '/imports/api/base';
 import '/imports/api/profile';
 import '/imports/api/interest';
+
+import '../imports/ui/stylesheets/style.css';
